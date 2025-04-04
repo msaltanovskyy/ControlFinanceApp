ae731fcc68304f6a42f4e18f8c836e2ba582c8bf
msaltanovskyy <bust.cot@gmail.com>
2025-04-04 11:58:58 +0200

    Add user management routes and controller methods for editing, deactivating, and retrieving users

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 const { model } = require("mongoose");
+const { get } = require("../routes/userRoutes");
 
 const registerUser = async (req, res) => {
     res.json({ message: "Register User" });
@@ -12,8 +13,28 @@ const logoutUser = async (req, res) => {
     res.json({ message: "Logout User" });
 }
 
+const editUser = async (req, res) => {
+    res.json({ message: "Edit User" });
+}
+const deactiveUser = async (req, res) => {
+    res.json({ message: "Deactive User" });
+}
+
+const getUser = async (req, res) => {
+    res.json({ message: "Get User" });
+}
+
+const getAllUsers = async (req, res) => {
+    const users = await User.find();
+    res.json(users);
+}
+
 module.exports = {  
     registerUser,
     loginUser,
-    logoutUser
+    logoutUser,
+    editUser,
+    deactiveUser,
+    getUser,
+    getAllUsers
 }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,11 +4,23 @@ const router = express.Router();
 const {
     registerUser,
     loginUser,
-    logoutUser
+    logoutUser,
+    editUser,
+    deactiveUser,
+    getUser,
+    getAllUsers
 } = require('../controllers/userController'); // Import user controller
 
+//POST routes
 router.post('/register', registerUser); // Register user route
 router.post('/login', loginUser); // Login user route
-router.get('/logout', logoutUser); // Logout user route
+router.post('/logout', logoutUser); // Logout user route
+router.post('/edit/:id',editUser); // Edit user route
+router.post('/deactive/:id', deactiveUser); // Deactive user route
+
+//GET routes
+router.get('/:id', getUser); // Get user route
+router.get('/', getAllUsers); // Get all users route
+
 
 module.exports = router; // Export user routes
